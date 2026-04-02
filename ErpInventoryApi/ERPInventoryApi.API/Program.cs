864f8154f17d84dce23c02d0655bc7c9d9d4aefa
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-04-01 23:24:56 -0400

    Docker Compose updated

--- ErpInventoryApi/ERPInventoryApi.API/Program.cs
+++ ErpInventoryApi/ERPInventoryApi.API/Program.cs
@@ -1,23 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using ERPInventoryApi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using Serilog;
 
-// Add services to the container.
+Log. Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    Log.Information("Starting ERP Inventory API....");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    //use serial logg for all logging
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .WriteTo.Console());
+
+    //Add Service
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddOpenApi();
+
+    // PostgreSQL + EF Core
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    // Health checks
+    builder.Services.AddHealthChecks()
+        .AddDbContextCheck<AppDbContext>();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    app.UseSerilogRequestLogging();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.MapHealthChecks("/health");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally 
+{
+    Log.CloseAndFlush();
+}
